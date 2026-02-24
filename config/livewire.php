e80d8830d686e4eb909efa23d7c87b059060dd0f
Ishtiak Ahmed <ishtiakahmed347@gmail.com>
2026-02-24 23:11:43 +0600

    Fix: Override Livewire upload config to allow all image types (fixes iOS HEIC/422 error)

---
+++ config/livewire.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'temporary_file_upload' => [
+        'disk' => null,
+        'rules' => ['required', 'file', 'max:20480'], // 20MB max, no MIME restrictions
+        'directory' => null,
+        'middleware' => 'throttle:60,1',
+        'preview_mimes' => [
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        ],
+        'max_upload_time' => 5,
+        'cleanup' => true,
+    ],
+    'temporary_file_upload_disk' => null,
+];
